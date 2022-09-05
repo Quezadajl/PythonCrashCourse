5ed4662554f8da023cc26e339dff6a0180ec7d0a
Quezadajl <guicho.21.jq@gmail.com>
2022-09-05 10:17:45 -0500

    python_work

--- python_work/full_name.py
+++ python_work/full_name.py
@@ -2,4 +2,7 @@ first_name = "ada"
 last_name = "lovalace"
 full_name = f"{first_name} {last_name}"
 	#f-strings, f is for format.Python will replace each variable with its value to use inside the string displayed.
-print(f"Hello, {full_name.title()}!")
+#print(f"Hello, {full_name.title()}!")
+	#f-strings allow you to compose complete messages
+message = f"Hello, {full_name.title()}!"
+print(message)
