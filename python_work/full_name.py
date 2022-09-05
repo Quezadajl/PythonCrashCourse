e8a86f6cb7c0438755be72cb969c52e8809fef2a
Quezadajl <guicho.21.jq@gmail.com>
2022-09-05 10:38:29 -0500

    python_work

--- python_work/full_name.py
+++ python_work/full_name.py
@@ -13,4 +13,6 @@ message = f"See you later, {full_name.title()}!"
 print("Python")
 print("\tPython") #applying a tab to my code with \t
 
-print("Languages: \t \nPython\nC\nJavaScript")
+print("Languages: \n\tPython\nC\nJavaScript") #applying a newline with \n
+	#additionally combine them to \n & \t for new lines and tab the statement
+
