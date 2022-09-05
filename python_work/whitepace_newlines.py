045c5c69719fc1443450d7be49bc3e8a891fc82f
Quezadajl <guicho.21.jq@gmail.com>
2022-09-05 10:41:35 -0500

    python_work

---
+++ python_work/whitepace_newlines.py
@@ -0,0 +1,6 @@
+#Adding Whitespace to Strings with Tabs or Newlines
+print("Python")
+print("\tPython") #applying a tab to my code with \t
+
+print("Languages: \n\tPython\nC\nJavaScript") #applying a newline with \n
+	#additionally combine them to \n & \t for new lines and tab the statement
