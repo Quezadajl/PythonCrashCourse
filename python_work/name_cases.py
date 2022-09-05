7075c08a90c8b1fb7e42fab19b7a076b7d531567
Quezadajl <guicho.21.jq@gmail.com>
2022-09-05 11:29:05 -0500

    python_work

---
+++ python_work/name_cases.py
@@ -0,0 +1,6 @@
+ #Personal Message
+first_born = "Sophie"
+second_born = "Martin"
+third_born = "Lola"
+my_babies = f"Hello love of my life {first_born}, {second_born}, and {third_born}!"
+print(my_babies)
