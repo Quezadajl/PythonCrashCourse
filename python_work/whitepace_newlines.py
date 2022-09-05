60eb0096fff52ef9818a4846e79212d8bd9bcef0
Quezadajl <guicho.21.jq@gmail.com>
2022-09-05 10:47:46 -0500

    python_work

--- python_work/whitepace_newlines.py
+++ python_work/whitepace_newlines.py
@@ -4,3 +4,8 @@ print("\tPython") #applying a tab to my code with \t
 
 print("Languages: \n\tPython\nC\nJavaScript") #applying a newline with \n
 	#additionally combine them to \n & \t for new lines and tab the statement
+
+
+#Stripping whitespace
+favorite_language = 'python '
+print(favorite_language.rstrip(favorite_language))
