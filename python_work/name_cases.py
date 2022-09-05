12cf51e79fd5193c4a46bc8508f367baa1271698
Quezadajl <guicho.21.jq@gmail.com>
2022-09-05 11:31:55 -0500

    pyton_work

--- python_work/name_cases.py
+++ python_work/name_cases.py
@@ -4,3 +4,9 @@ second_born = "Martin"
 third_born = "Lola"
 my_babies = f"Hello love of my life {first_born}, {second_born}, and {third_born}!"
 print(my_babies)
+
+#Name cases
+first_name = "Tania"
+last_name = "Davila"
+full_name = f"{first_name} {last_name}"
+print(full_name.title())
