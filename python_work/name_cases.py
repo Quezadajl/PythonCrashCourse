52a5271af12bcc4b8b508970855f8f6d4da7fd82
Quezadajl <guicho.21.jq@gmail.com>
2022-09-05 11:38:38 -0500

    python_work

--- python_work/name_cases.py
+++ python_work/name_cases.py
@@ -12,3 +12,9 @@ full_name = f"{first_name} {last_name}"
 print(full_name.upper())
 print(full_name.lower())
 print(full_name.title())
+
+#FamousQuote
+author = "Thor"
+quote = "I am still worthy!"
+motivation = f"{quote}!? - {author}"
+print(motivation)
