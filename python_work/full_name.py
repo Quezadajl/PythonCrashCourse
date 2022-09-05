f703efe15fa5c3a697b8956de7cc5445c5c9e5eb
Quezadajl <guicho.21.jq@gmail.com>
2022-09-05 10:40:23 -0500

    python_work

--- python_work/full_name.py
+++ python_work/full_name.py
@@ -2,17 +2,8 @@ first_name = "ada"
 last_name = "lovalace"
 full_name = f"{first_name} {last_name}"
 	#f-strings, f is for format.Python will replace each variable with its value to use inside the string displayed.
-#print(f"Hello, {full_name.title()}!")
+print(f"Hello, {full_name.title()}!")
 	#f-strings allow you to compose complete messages
 message = f"See you later, {full_name.title()}!"
-#print(message)
-
-
-
-#Adding Whitespace to Strings with Tabs or Newlines
-print("Python")
-print("\tPython") #applying a tab to my code with \t
-
-print("Languages: \n\tPython\nC\nJavaScript") #applying a newline with \n
-	#additionally combine them to \n & \t for new lines and tab the statement
+print(message)
 
