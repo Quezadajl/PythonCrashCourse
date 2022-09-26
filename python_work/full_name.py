5314d9f02805044f6f1b70c23477dc75f339b32f
Quezadajl <guicho.21.jq@gmail.com>
2022-09-26 17:47:38 -0500

    python_work

--- python_work/full_name.py
+++ python_work/full_name.py
@@ -1,5 +1,5 @@
-first_name = "ada"
-last_name = "lovalace"
+first_name = "Joselit"
+last_name = "Questlove"
 full_name = f"{first_name} {last_name}"
 	#f-strings, f is for format.Python will replace each variable with its value to use inside the string displayed.
 print(f"Hello, {full_name.title()}!")
@@ -7,3 +7,5 @@ print(f"Hello, {full_name.title()}!")
 message = f"See you later, {full_name.title()}!"
 print(message)
 
+message2= f"Work on yourself, {full_name.title()}!!!"
+print(message2)
