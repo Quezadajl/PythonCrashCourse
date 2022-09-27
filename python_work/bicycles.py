30f7d3de3596b181f09c9a5987293ab8b7a1c38f
Quezadajl <guicho.21.jq@gmail.com>
2022-09-27 18:25:41 -0500

    python_work

--- python_work/bicycles.py
+++ python_work/bicycles.py
@@ -2,3 +2,5 @@ bicycles = ['trek','cannondale','redline', 'specialized']
 print(bicycles)
 print(bicycles[0])
 #accessing any element in a list by telling python the position, or INDEX starting at 0
+#mix index with formating
+print(bicycles[0].title())
