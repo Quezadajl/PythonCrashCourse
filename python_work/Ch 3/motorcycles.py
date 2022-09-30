9b6f6163af792f150e234484a2be4c10f32fb596
Quezadajl <guicho.21.jq@gmail.com>
2022-09-29 19:13:58 -0500

    python_work

--- python_work/Ch 3/motorcycles.py
+++ python_work/Ch 3/motorcycles.py
@@ -3,6 +3,6 @@ print(motorcycles)
 #appending to the end of a list
 motorcycles.append('ducati')
 print(motorcycles)
-#changing, adding and removing elements in a list; makes it easier to build list dynamically
+#changing, adding and removing elements in a list; makes it easier to build lists dynamically
 motorcycles[0] = 'BMW'
 print(motorcycles)#the code shows that we replaced the first element by re-defining a new list
