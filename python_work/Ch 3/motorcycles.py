1c4a62425718c8aca65a83a6cfe8aed2a38d05c0
Quezadajl <guicho.21.jq@gmail.com>
2022-09-29 19:11:30 -0500

    python_work

--- python_work/Ch 3/motorcycles.py
+++ python_work/Ch 3/motorcycles.py
@@ -1,7 +1,8 @@
 motorcycles = ['honda','yamaha','suzuki']
 print(motorcycles)
 #appending to the end of a list
-
+motorcycles.append('ducati')
+print(motorcycles)
 #changing, adding and removing elements in a list
-motorcycles[0] = 'ducati'
+motorcycles[0] = 'BMW'
 print(motorcycles)#the code shows that we replaced the first element by re-defining a new list
