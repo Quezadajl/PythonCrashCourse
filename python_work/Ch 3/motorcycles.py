11ebfe7d5f488970f88889f42a97631080f95d32
Quezadajl <guicho.21.jq@gmail.com>
2022-10-04 18:37:10 -0500

    python_work

--- python_work/Ch 3/motorcycles.py
+++ python_work/Ch 3/motorcycles.py
@@ -6,3 +6,10 @@ print(motorcycles)
 #changing, adding and removing elements in a list; makes it easier to build lists dynamically
 motorcycles[0] = 'BMW'
 print(motorcycles)#the code shows that we replaced the first element by re-defining a new list
+#Inserting Elements into a List
+my_doggies = []
+my_doggies.append('Martin')
+my_doggies.append('Sophie')
+my_doggies.append('Alolamola')
+
+print(my_doggies)
