efd87638e1e9a2e91c0838ddb97cd941318c07f5
Quezadajl <guicho.21.jq@gmail.com>
2022-10-17 18:18:16 -0500

    python_work

--- python_work/Ch 3/motorcycles.py
+++ python_work/Ch 3/motorcycles.py
@@ -42,5 +42,7 @@ print(motorcycles)
 
 #Sometimes you won't know the position of the value you want to remove from a list. If you only know the value of the item you want..
 #...to remove, you can use the remove() method
-motorcycles.remove('ducati')
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive) #we assiged the value to a variable; we then use this variable to tell python which value to remove
 print(motorcycles)
+print(f'\nA {too_expensive.title()} is too expensive for me.')
