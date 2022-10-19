ef93b84812b353234f5cad5b62ebcd43d41e4670
Quezadajl <guicho.21.jq@gmail.com>
2022-10-19 17:16:18 -0500

    python_work

--- python_work/Ch 3/car.py
+++ python_work/Ch 3/car.py
@@ -1,4 +1,6 @@
-cars = ['bmw', 'audi', 'Toyota', 'subaru'] #upper or lower case can impact how the list is sorted
+cars = ['bmw', 'audi', 'toyota', 'subaru'] #upper or lower case can impact how the list is sorted
 cars.sort()
 print(cars)
 #Python's sort() method makes it relatively easy to sort a list
+cars.sort(reverse=True)
+print(cars)
