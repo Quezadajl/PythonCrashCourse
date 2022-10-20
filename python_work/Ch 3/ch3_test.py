a152f7cadd62fcf83896d10572c35d54bc8821bf
Quezadajl <guicho.21.jq@gmail.com>
2022-10-20 17:39:27 -0500

    python_work

--- python_work/Ch 3/ch3_test.py
+++ python_work/Ch 3/ch3_test.py
@@ -1,3 +1,6 @@
 #Try it yourself review for Chapter 3
 world_sites = []
-world_sites.append('')
+world_sites.append('Thailand')
+world_sites.append('Hawaii')
+world_sites.append('Venice')
+print(world_sites)
