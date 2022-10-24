77718bc6eaadd2a241637a828d3d9b2b122aed7a
Quezadajl <guicho.21.jq@gmail.com>
2022-10-24 17:35:38 -0500

    python_work

--- python_work/Ch 4/magicians.py
+++ python_work/Ch 4/magicians.py
@@ -1,3 +1,5 @@
-magicians = ['alice','david','carolina']
-for magician in magicians:
+magicians = ['alice','david','carolina'] #Defining the list
+for magician in magicians: #use the for loop as "for every magician in the list of magicians, print the magician's name"
 	print(magician)
+# We tell python to print the name that's just been assigned to the variable magician.
+
