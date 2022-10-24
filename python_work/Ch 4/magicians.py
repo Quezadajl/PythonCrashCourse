2284aa6739b72121772e2390c05c67b698736735
Quezadajl <guicho.21.jq@gmail.com>
2022-10-24 18:02:05 -0500

    python_work

--- python_work/Ch 4/magicians.py
+++ python_work/Ch 4/magicians.py
@@ -11,3 +11,4 @@ magicians = ['alice','david','carolina'] #Defining the list
 for magician in magicians: #use the for loop as "for every magician in the list of magicians, print the magician's name"
 	print(f'{magician.title()}, that was a great trick!')
 	print(f"I can't wait to see your next trick, {magician.title()}.\n")
+print("Thank you, everyone. That was a great magic show!")# Using print without the indentation does not go with the loop
