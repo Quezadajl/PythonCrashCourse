054c1a63e63c726cce4fc848376b9b08fa0d4f16
Quezadajl <guicho.21.jq@gmail.com>
2022-10-24 17:47:06 -0500

    python_work

--- python_work/Ch 4/magicians.py
+++ python_work/Ch 4/magicians.py
@@ -10,3 +10,4 @@ for doggies in babies:#doggies is the variable I assigned to each print of the l
 magicians = ['alice','david','carolina'] #Defining the list
 for magician in magicians: #use the for loop as "for every magician in the list of magicians, print the magician's name"
 	print(f'{magician.title()}, that was a great trick!')
+	print(f"I can't wait to see your next trick, {magician.title()}.\n")
