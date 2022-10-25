c5ebd8b9ebd1f8f0f356817ceeb4e09615258807
Quezadajl <guicho.21.jq@gmail.com>
2022-10-25 18:20:01 -0500

    python_work

--- python_work/Ch 4/squares.py
+++ python_work/Ch 4/squares.py
@@ -1,6 +1,6 @@
-squares = []
-for value in range(1,11):
-	square = value**2
-	squares.append(square)
+squares = [] #Start with an empty list called squares
+for value in range(1,11): #We tell Python to loop through each value from 1 to 10 using the range() function
+	square = value**2 #Inside the loop, the current value is raised to the 2nd power
+	squares.append(square) #each new value will be raised to the 2nd square
 
 print(squares)
