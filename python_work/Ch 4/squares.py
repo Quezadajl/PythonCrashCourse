220d74142133691b4650806b72a8a156c388eb23
Quezadajl <guicho.21.jq@gmail.com>
2022-10-25 18:21:47 -0500

    python_work

--- python_work/Ch 4/squares.py
+++ python_work/Ch 4/squares.py
@@ -4,3 +4,9 @@ for value in range(1,11): #We tell Python to loop through each value from 1 to 1
 	squares.append(square) #each new value will be raised to the 2nd square
 
 print(squares)
+
+#Now for the clean-high level version of the above
+squares = []
+for value in range(1,11):
+	squares.append(value**2)
+print(squares)
