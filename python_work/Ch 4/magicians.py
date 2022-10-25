e759a659acdb8797d097d311e1346167d216a25a
Quezadajl <guicho.21.jq@gmail.com>
2022-10-25 17:33:53 -0500

    python_work

--- python_work/Ch 4/magicians.py
+++ python_work/Ch 4/magicians.py
@@ -12,3 +12,8 @@ for magician in magicians: #use the for loop as "for every magician in the list 
 	print(f'{magician.title()}, that was a great trick!')
 	print(f"I can't wait to see your next trick, {magician.title()}.\n")
 print("Thank you, everyone. That was a great magic show!")# Using print without the indentation does not go with the loop
+
+#4.1 Challenge
+pizza = ['pepperoni', 'pinapple','cheese']
+for flavors in pizza:
+	print(flavors)
