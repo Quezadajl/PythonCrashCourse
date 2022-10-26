7324782de9a478ddc6d1219709fc9b1f9561154d
Quezadajl <guicho.21.jq@gmail.com>
2022-10-26 18:26:01 -0500

    python_work

--- python_work/Ch 4/try_it_challenges.py
+++ python_work/Ch 4/try_it_challenges.py
@@ -2,20 +2,29 @@
 count = [value for value in range(1,21)]
 #print(count)
 
-#4.4
+#4.4 & 4.5
 milli = [value for value in list(range(1,1000001))]
 #print(milli)
 #print(min(milli))
 #print(max(milli))
 #print(sum(milli))
 
-#4.5
+#4.6
 odd = [value for value in range(1,21,2)]
 print(odd)
 
 for value in range(1,21,2):
 	print(value)
 
-#4.6 & 4.7
+#4.7 & 4.8
 threes = [value for value in range(3,33,3)]
 print(threes)
+
+#4.9 & 4.10
+cubes = [value**3 for value in range(1,11)]
+print(cubes)
+
+cubes_2 = []
+for cube in range(1,11):
+	cubes_2.append(cube**3)
+print(cubes_2)
