f400afb89cfb7288992dd3ec053903e0b53ee257
Quezadajl <guicho.21.jq@gmail.com>
2022-10-26 18:05:04 -0500

    python_work

--- python_work/Ch 4/try_it_challenges.py
+++ python_work/Ch 4/try_it_challenges.py
@@ -3,5 +3,8 @@ count = [value for value in range(1,21)]
 print(count)
 
 #4.4
-milli = [value for value in list(range(1,1000000,100000))]
-print(milli)
+milli = [value for value in list(range(1,1000001))]
+#print(milli)
+print(min(milli))
+print(max(milli))
+print(sum(milli))
