3c2a1d17d2eecbb3b5272d5bccc68d07bae44a87
Quezadajl <guicho.21.jq@gmail.com>
2022-10-26 18:12:44 -0500

    python_work

--- python_work/Ch 4/try_it_challenges.py
+++ python_work/Ch 4/try_it_challenges.py
@@ -1,10 +1,21 @@
 #4.3
 count = [value for value in range(1,21)]
-print(count)
+#print(count)
 
 #4.4
 milli = [value for value in list(range(1,1000001))]
 #print(milli)
-print(min(milli))
-print(max(milli))
-print(sum(milli))
+#print(min(milli))
+#print(max(milli))
+#print(sum(milli))
+
+#4.5
+odd = [value for value in range(1,21,2)]
+print(odd)
+
+for value in range(1,21,2):
+	print(value)
+
+#4.6 & 4.7
+threes = [value for value in range(3,33,3)]
+print(threes)
