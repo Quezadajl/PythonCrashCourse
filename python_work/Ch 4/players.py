28a2312f240478389fce4a3aca2ae423ee9042cc
Quezadajl <guicho.21.jq@gmail.com>
2022-10-27 18:24:55 -0500

    python_work

--- python_work/Ch 4/players.py
+++ python_work/Ch 4/players.py
@@ -4,5 +4,13 @@ print(players[0:3]) #Here we recall the first three elements
 print(players[:4]) # omitting the first index, python starts from the first element
 print(players[2:])# you can also start with the third element and index all the way through
 print(players[-3:])#using a negative pulls the last elements of the list
-print(players[0:3:2])
+print(players[0:3:2])#A third number allows you to skip slice the list
 print(players)
+
+
+#Using Looping through a slice
+names = ['martin', 'sophie', 'lola', 'ash', 'taylor','chikorita']
+print("Here are the first three players on my team:")
+for player in names[:3]:
+	print(player.title())
+	#Instead of looping through the entire list, Python loops through only the first three names
