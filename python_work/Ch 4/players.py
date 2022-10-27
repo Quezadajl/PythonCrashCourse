5862817f6a89f21fe8b4fa2f82c791cf1fb489d5
Quezadajl <guicho.21.jq@gmail.com>
2022-10-27 18:15:45 -0500

    python_work

--- python_work/Ch 4/players.py
+++ python_work/Ch 4/players.py
@@ -2,4 +2,7 @@
 players = ['charles', 'martina','michael','florence','eli']
 print(players[0:3]) #Here we recall the first three elements
 print(players[:4]) # omitting the first index, python starts from the first element
-print(players[1:])
+print(players[2:])# you can also start with the third element and index all the way through
+print(players[-3:])#using a negative pulls the last elements of the list
+print(players[0:3:2])
+print(players)
