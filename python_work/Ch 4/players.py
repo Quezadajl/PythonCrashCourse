1441c8b3262f44e0ad44a59e66c8f00f0e2520ab
Quezadajl <guicho.21.jq@gmail.com>
2022-10-27 18:05:10 -0500

    python_work

--- python_work/Ch 4/players.py
+++ python_work/Ch 4/players.py
@@ -1,3 +1,5 @@
 #Slicing, outputting a specific elements in a list
 players = ['charles', 'martina','michael','florence','eli']
-print(players[0:3])
+print(players[0:3]) #Here we recall the first three elements
+print(players[:4]) # omitting the first index, python starts from the first element
+print(players[1:])
