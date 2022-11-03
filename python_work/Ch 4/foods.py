8cca68badbd7ca1d8cd4240eae2eabac35b8a42b
Quezadajl <guicho.21.jq@gmail.com>
2022-11-03 18:24:25 -0500

    python_work

--- python_work/Ch 4/foods.py
+++ python_work/Ch 4/foods.py
@@ -1,10 +1,9 @@
 my_foods = ['pizza','thai curry','tinga']
-
+print(my_foods)
 my_foods.insert(0,'briyani') #Insert added biriyani to where ever you appointed it to
+my_foods[0] = ('tofu')
 friend_foods = my_foods[:] #Using the colon with square brackets copies everything on the list for reusing a list
 #We make a cop by asking for a slice w/o specifying any indices and store the copy list
-
-
 
 print("My favorite food are:")
 print(my_foods)
@@ -14,9 +13,9 @@ print(friend_foods)
 
 my_foods.append('Pho')
 friend_foods.append('pupusas')
-
+#These changes show that we have two different list that we can keep adding things to them individually 
 print("My favorite food are:")
 print(my_foods)
 
-print("\nMy frined's favorite foods are:")
+print("\nMy friend's favorite foods are:")
 print(friend_foods)
