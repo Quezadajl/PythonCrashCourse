dc22a228ade93d0f55e5cba497285fd8c11bd17e
Quezadajl <guicho.21.jq@gmail.com>
2022-11-07 17:58:00 -0600

    python_work

---
+++ python_work/Ch 4/dimensions.py
@@ -0,0 +1,5 @@
+#Tuples - python refers to values that cannot change as immutable, and an immutable list is called a tuple
+#Tuples can still be indexed/sliced
+dimensions = (200, 50) # Tuples use parantheses instead of square brackets
+print(dimensions[0])
+print(dimensions[1])
