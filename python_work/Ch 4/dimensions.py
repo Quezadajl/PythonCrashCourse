721395647936a295e0b2d45c9fae22a162096d47
Quezadajl <guicho.21.jq@gmail.com>
2022-11-07 18:00:50 -0600

    python_work

--- python_work/Ch 4/dimensions.py
+++ python_work/Ch 4/dimensions.py
@@ -3,3 +3,7 @@
 dimensions = (200, 50) # Tuples use parantheses instead of square brackets
 print(dimensions[0])
 print(dimensions[1])
+print(dimensions[:1])
+#Lets try to change one of the items
+dimensions[0] = 150
+print(dimensions)#error code reads : 'tuple' object does not support item assignment
