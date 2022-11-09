a2277a4db419f87e4e0d94a64ebebe8180637090
Quezadajl <guicho.21.jq@gmail.com>
2022-11-08 18:01:38 -0600

    python_work

--- python_work/Ch 4/dimensions.py
+++ python_work/Ch 4/dimensions.py
@@ -25,7 +25,8 @@ for dimension in dimensions:
 	print(dimension)
 
 #Challenge 4-13
-buffet = ('rice', 'beans', 'guac', 'salsa', 'crema')
+buffet = ('rice', 'beans', 'guac', 'salsa', 'crema')#Create a tuple of 5 foods at a buffet
 print(buffet)
-for meal in buffet:
+for meal in buffet: #Create a for loop to print each item
 	print(meal)
+buffet[3] = ('fajitas') #tried to modify a tuple
