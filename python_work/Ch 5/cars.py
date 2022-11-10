2202878a6cff35f2fa4669ff179abb72007fe2d1
Quezadajl <guicho.21.jq@gmail.com>
2022-11-09 18:19:15 -0600

    python_work

--- python_work/Ch 5/cars.py
+++ python_work/Ch 5/cars.py
@@ -1,4 +1,8 @@
 #Intro to if statements in python
 cars = ['audi','bmw', 'subaru','toyota']
+
 for car in cars:
-	print(car.upper())
+	if car == 'bmw':#if the value of car is 'bmw'upper case
+		print(car.upper())
+	else:
+		print(car.title())#if the value of car is anthing othar than 'bmw', title case it
