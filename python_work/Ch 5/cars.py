03f905c06583c59c8879fd096a610ab9085fc65a
Quezadajl <guicho.21.jq@gmail.com>
2022-11-17 18:10:23 -0600

    python_work

--- python_work/Ch 5/cars.py
+++ python_work/Ch 5/cars.py
@@ -32,9 +32,18 @@ for vehicle in auto:
 	else:
 		print(False)
 
-next_chapter = print(f'\nCheckin for inequalities: Next Section\n')
+next_section= print(f'\nCheckin for inequalities: Next Section\n')#toppings.py
 
-requested_topping = 'mushrooms'
+requested_topping = 'mushrooms' 
 
-if requested_topping != 'anchovies':
-	print("Hold the anchovies!")
+if requested_topping != 'anchovies': #This line compares the value or requested_topping ot the value 'anchovies'
+	print("Hold the anchovies!")#If False, Python returns the print statement,True otherwise
+
+next_section= print(f'\nCheckin for inequalities: Next Section\n')#magic_number.py
+
+answer = 17
+if answer != 42:
+	print("That is not the correct answer. Please Try again!\n")
+#Teh conditional test passes, since the answer is not = 17, the intended code block is executed
+
+
