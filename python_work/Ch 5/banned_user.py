dd3e09623779a60cfd036a3184ca682e0bc9b5dc
Quezadajl <guicho.21.jq@gmail.com>
2022-11-21 18:00:45 -0600

    python_work;

--- python_work/Ch 5/banned_user.py
+++ python_work/Ch 5/banned_user.py
@@ -1,5 +1,7 @@
 banned_users = ['andrew','carolina','david']
-user = 'Andrew'
+user = 'david'
+user = 'marie'
 
 if user not in banned_users:
 	print(f'{user.title()}, you can post a response if you wish.')
+# If teh value user is not in the list banned_users, Python returns True and executes the intended line.
