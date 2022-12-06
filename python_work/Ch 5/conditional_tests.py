98ba617c9855434a09778b4d24a9c8d25d4c8ca4
Quezadajl <guicho.21.jq@gmail.com>
2022-12-05 18:21:51 -0600

    python_work

---
+++ python_work/Ch 5/conditional_tests.py
@@ -0,0 +1,25 @@
+#Creating a list of conditional test to practice: 5-2
+# Testing equality && inequality
+desserts = ['ice cream', 'chocolate', 'apple crisp', 'cookies']
+favorite_dessert = 'apple crisp'
+
+# Print the desserts out, but let everyone know my favorite dessert.
+for dessert in desserts:
+    if dessert == favorite_dessert:
+        # This dessert is my favorite, let's let everyone know!
+        print("%s is my favorite dessert!" % dessert.title())
+    else:
+        # I like these desserts, but they are not my favorite.
+        print("I like %s." % dessert)
+
+snacks = ['peanuts','chocolate','doritos','popcorn']
+favorite_snack = 'doritos'
+
+#lets print snacks out, but let everyone know my favorite snack
+for snack in snacks:
+	if snack == favorite_snack:
+		#this snack is my favorite, let's let everyone know!
+		print("%s is my favorite snack!" % snack.title()) # -- it looks like the % is used as a variable with additional formating
+	else:
+		#I like these other snacks, but not my favorite
+		print("I like %s." % snack)
