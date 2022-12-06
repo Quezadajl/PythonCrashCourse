722ecc1459084238e009d1105c5e6567a7e0d857
Quezadajl <guicho.21.jq@gmail.com>
2022-12-05 18:02:55 -0600

    python_work

--- python_work/Ch 5/banned_user.py
+++ python_work/Ch 5/banned_user.py
@@ -5,3 +5,10 @@ user = 'marie'
 if user not in banned_users:
 	print(f'{user.title()}, you can post a response if you wish.')
 # If teh value user is not in the list banned_users, Python returns True and executes the intended line.
+
+#Coditional Test 5-1
+car = 'Subaru'
+print("is car == 'subaru'? I predict True.")
+print(car == 'Subaru') # Case is important with conditional tests, 
+print("\nIs car == 'audi'? I predict False")
+print(car == 'audi')
