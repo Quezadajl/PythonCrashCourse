249fdd1a9bdf94b2742d6da9f3df33dbd18a3cce
Quezadajl <guicho.21.jq@gmail.com>
2022-12-05 19:09:16 -0600

    python_work

--- python_work/Ch 5/conditional_tests.py
+++ python_work/Ch 5/conditional_tests.py
@@ -17,6 +17,13 @@ requested_topping = 'pineapple'
 if requested_topping != 'jalapenos':
 	print("Hold the jalapenos!")
 
-#Checking if an item is in a list
-vowels = ['a', 'e', 'i', 'o', 'u']
-'a' in vowels
+needed_ingredients = ['doritos','popcorn','chocolate','fruit'] #Here I'm checking if these items are in the pantry
+avail_ingredient = 'chocolate'
+
+for treat in needed_ingredients:
+	if treat == avail_ingredient:
+		#this snack is my favorite, let's let everyone know!
+		print("Great we have %s!" % treat.title()) # -- it looks like the % is used as a variable with additional formating
+	else:
+		#I like these other snacks, but not my favorite
+		print("We will need %s from the store." % treat)
