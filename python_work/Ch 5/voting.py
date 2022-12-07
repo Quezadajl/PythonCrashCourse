7779832288c9ca93737db8742ee6b050df32b864
Quezadajl <guicho.21.jq@gmail.com>
2022-12-07 17:38:44 -0600

    python_work

--- python_work/Ch 5/voting.py
+++ python_work/Ch 5/voting.py
@@ -1,7 +1,10 @@
 age = 19
-age = 17
+#age = 17
 if age >= 18:
-	print('You are old enough to vote')
+	print('You are old enough to vote!')
+	print('Have you registered to vote yet?')
 else:
 	print('Sorry you are too young to vote')
-	print('Pleae register to vote when you turn 18!')
+	print('Pleae register to vote when you turn 18!')#All indented lines after an if statement will execute
+
+#Simple conditional tests: if conditional test: do something
