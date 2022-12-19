20b92723d9f91d2b23e1ae9f62898cdc724814f5
Quezadajl <guicho.21.jq@gmail.com>
2022-12-19 17:31:38 -0600

    python_work

--- python_work/Ch 5/voting.py
+++ python_work/Ch 5/voting.py
@@ -86,14 +86,14 @@ if 'mango' in favorite_fruits:
 
 #Testing Multiple conditions AND ALL of them have to pass!!: TOPPINGS.PY
 requested_toppings = ['mushrooms','green peppers','extra cheese']
-#if 'mushrooms' in requested_toppings:
-	#print('Adding mushrooms.')
-#if 'pepperoni' in requested_toppings:
-	#print('Adding pepperoni.')
-#if 'extra cheese' in requested_toppings:
-	#print('Adding extra cheese.')
+if 'mushrooms' in requested_toppings:
+	print('Adding mushrooms.')
+if 'pepperoni' in requested_toppings:
+	print('Adding pepperoni.')
+if 'extra cheese' in requested_toppings:
+	print('Adding extra cheese.')
 
-#print("\nFinished making your pizza!")*/
+print("\nFinished making your pizza!")
 requested_toppings = []
 
 if requested_toppings:
@@ -102,7 +102,7 @@ if requested_toppings:
 			print("Sorry, we are out of green peppers right now.")
 		else:
 			print(f"Adding {requested_topping}.")
-	print("\nFinished making your pizza!")
+	print("\nFinished making your pizza!!")
 else:
 	print("Are you sure you want a plain pizza?")
 
@@ -116,5 +116,5 @@ for requested_topping in requested_toppings:
 		print(f"Adding {requested_topping}.")
 	else:
 		print(f"Sorry, we don't have {requested_topping}.")
-print("\nFinished making your pizza!!")
+print("\nFinished making your pizza!!!")
 
