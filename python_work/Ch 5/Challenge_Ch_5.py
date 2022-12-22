6390a4d2464bb6558a639b8c70afb6136a746edf
Quezadajl <guicho.21.jq@gmail.com>
2022-12-21 18:54:27 -0600

    python_work

--- python_work/Ch 5/Challenge_Ch_5.py
+++ python_work/Ch 5/Challenge_Ch_5.py
@@ -1,16 +1,12 @@
 #Hello Admin challenge
 usernames = ['totodile','Cyndaquil','Chikorita','Rowlet','Admin']
 
-for username in usernames:
-	if usernames == 'Admin':
-		print("Hello admin, would you like to see a status report?")
-	else:
-		print(f"Hello thank you for logging in again.")
-	#print("\nFinished making your pizza!!")
-
-not_admin = ['Green','Yellow','Red']
-admin = ['admin']
-if not_admin in not_admin:
-	print('Hello thank you for logging in again.')
-if admin == ['admin']:
+usernames = ['totodile','Cyndaquil','Chikorita','Rowlet','Admin']
+if 'totodile' in usernames:
+	print(f'Hello {usernames[0]}, thank you for logging in again.')
+if 'Cyndaquil' in usernames:
+	print(f'Hello {usernames[1]}, thank you for logging in again.')
+if 'Rowlet' in usernames:
+	print(f'Hello {usernames[2]}, thank you for logging in again.')
+if 'Admin' in usernames:
 	print('Hello admin, would you like to see a status report?')
