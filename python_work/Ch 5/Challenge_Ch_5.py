b3b2b0fac43433c9fd6ec67bf7b0d85ca0004b24
Quezadajl <guicho.21.jq@gmail.com>
2022-12-21 18:41:32 -0600

    python_work

--- python_work/Ch 5/Challenge_Ch_5.py
+++ python_work/Ch 5/Challenge_Ch_5.py
@@ -1,2 +1,11 @@
 #Hello Admin challenge
-usernames = ['totodile']
+usernames = ['totodile','Cyndaquil','Chikorita','Rowlet','Admin']
+if usernames:
+	for username in usernames:
+		if usernames == 'Admin':
+			print("Hello admin, would you like to see a status report?")
+		else:
+			print(f"Adding {requested_topping}.")
+	print("\nFinished making your pizza!!")
+else:
+	print("Are you sure you want a plain pizza?")
