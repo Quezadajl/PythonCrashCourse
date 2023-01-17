ca8900ed09e7c6c6e78ab3fcfefb9aa686a26c89
Quezadajl <guicho.21.jq@gmail.com>
2023-01-17 17:59:02 -0600

    python_work

--- python_work/Ch_5/Challenge_Ch_5.py
+++ python_work/Ch_5/Challenge_Ch_5.py
@@ -11,3 +11,8 @@ if 'Chimpchar' in usernames:
 	print(f'Hello!, thank you for logging in again')
 if 'Admin' in usernames:
 	print('Hello admin, would you like to see a status report?')
+
+if 'Admin' in usernames:
+	print('Hello Admin, would you like to see the status of the report')
+else:
+	print('Hello! thank you for logging in again!')
