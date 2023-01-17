422448899170dd655ed0693025fccb9ce7506f07
Quezadajl <taniadavila@Tanias-Air.attlocal.net>
2023-01-16 20:18:06 -0600

    python_work

---
+++ python_work/Ch_5/Challenge_Ch_5.py
@@ -0,0 +1,12 @@
+#Hello Admin challenge
+usernames = ['totodile','Cyndaquil','Chikorita','Rowlet','Admin']
+
+usernames = ['totodile','Cyndaquil','Chikorita','Rowlet','Admin']
+if 'totodile' in usernames:
+	print(f'Hello {usernames[0]}, thank you for logging in again.')
+if 'Cyndaquil' in usernames:
+	print(f'Hello {usernames[1]}, thank you for logging in again.')
+if 'Rowlet' in usernames:
+	print(f'Hello {usernames[2]}, thank you for logging in again.')
+if 'Admin' in usernames:
+	print('Hello admin, would you like to see a status report?')
