09172851dc8cec895abbdf1ff0c9c91733635285
Quezadajl <guicho.21.jq@gmail.com>
2023-01-17 17:42:34 -0600

    python_work

--- python_work/Ch_5/Challenge_Ch_5.py
+++ python_work/Ch_5/Challenge_Ch_5.py
@@ -1,12 +1,14 @@
 #Hello Admin challenge
-usernames = ['totodile','Cyndaquil','Chikorita','Rowlet','Admin']
+usernames = ['totodile','Cyndaquil','Chikorita','Rowlet','Admin','Chimpchar']
 
 usernames = ['totodile','Cyndaquil','Chikorita','Rowlet','Admin']
 if 'totodile' in usernames:
-	print(f'Hello {usernames[0]}, thank you for logging in again.')
+	print(f'Hello! thank you for logging in again.')
 if 'Cyndaquil' in usernames:
-	print(f'Hello {usernames[1]}, thank you for logging in again.')
+	print(f'Hello!, thank you for logging in again.')
 if 'Rowlet' in usernames:
-	print(f'Hello {usernames[2]}, thank you for logging in again.')
+	print(f'Hello! thank you for logging in again.')
+if 'Chimpchar' in usernames:
+	print(f'Hello!, thank you for logging in again')
 if 'Admin' in usernames:
 	print('Hello admin, would you like to see a status report?')
