37fe52c39b09069be0e6c81843c06533078494bd
Quezadajl <guicho.21.jq@gmail.com>
2023-01-17 18:08:10 -0600

    python_work

--- python_work/Ch_5/Challenge_Ch_5.py
+++ python_work/Ch_5/Challenge_Ch_5.py
@@ -1,6 +1,6 @@
 #Hello Admin challenge
 usernames = ['totodile','Cyndaquil','Chikorita','Rowlet','Admin','Chimpchar']
-root_user = ['Admins']
+root_user = ['Admin']
 
 if 'totodile' in usernames:
 	print(f'Hello! thank you for logging in again.')
@@ -13,8 +13,10 @@ if 'Chimpchar' in usernames:
 if 'Admin' in usernames:
 	print('Hello admin, would you like to see a status report?')
 
-#for root in root_user:
-	#if root in usernames:
-		#print('Hello Admin, would you like to see the status of the report')
-	#else:
-		#print('Hello! thank you for logging in again')
+for root in root_user:
+	if root in usernames:
+		print('Hello Admin, would you like to see the status of the report')
+	if root not in usernames:
+		print('Hello! thank you for logging in again')
+	else:
+		print('Hello! thank you for logging in again')
