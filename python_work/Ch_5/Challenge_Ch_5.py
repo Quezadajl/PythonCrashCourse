8693a2c8bb34a05b3319c5ba8d935060fd93ef1d
Quezadajl <guicho.21.jq@gmail.com>
2023-01-19 18:41:37 -0600

    python_work

--- python_work/Ch_5/Challenge_Ch_5.py
+++ python_work/Ch_5/Challenge_Ch_5.py
@@ -22,7 +22,7 @@ for root in root_user:
 		#print('Hello! thank you for logging in again')
 	else:
 		print('We need to find some users!')
-
+#Next Challenge
 current_users = ['Mario','Luigi','Pricess Peach','Toad','Yoshi']
 new_users = ['Mario','luigi','Princess Daisy', 'Rosalina','yoshi']
 
@@ -32,3 +32,17 @@ for new_user in new_users:
 		#helps make the output loop through each name individually and post it as its own line!!#mindblown
 	else:
 		print(new_user, 'is available')
+
+#Next challenge
+num = [1,2,3,4, 5, 6, 7, 8, 9]
+
+for number in num:
+	if number == 1:
+		print('1st')
+	elif number == 2:
+		print('2nd')
+	elif number == 3:
+		print('3rd')
+	elif number > 3:
+		print(str(number) + 'th')
+
