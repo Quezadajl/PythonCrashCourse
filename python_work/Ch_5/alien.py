0bb8960570c829d01985a24b257b2a73969d3189
Quezadajl <guicho.21.jq@gmail.com>
2023-01-23 22:09:37 -0600

    PythonCrashCourse

--- python_work/Ch_5/alien.py
+++ python_work/Ch_5/alien.py
@@ -1,2 +1,5 @@
 alien_0 = {'color':'green','points': 5} # using braces with semicolons to store information; Dict
 print(alien_0['color'])
+
+new_points = alien_0['points']
+print(f"You just earned {new_points} points!")
