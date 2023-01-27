94ae5ea0b8abe5705eaef19d64f5f9ac2a6a8e2f
Quezadajl <guicho.21.jq@gmail.com>
2023-01-27 17:56:11 -0600

    PythonCrashCourse

--- python_work/Ch_5/alien.py
+++ python_work/Ch_5/alien.py
@@ -17,5 +17,7 @@ alien_0['color'] = 'green'
 alien_0['points'] = 5
 
 print(alien_0)
+print(f"The alien is now {alien_0['color']}.")
 
-alien_0
+alien_0['color'] = 'yellow' #modifying values in a dictionary
+print(f"The alien is now {alien_0['color']}.")
