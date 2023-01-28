9c46596bafcc9bab1faf0bea836107f5bdefa841
Quezadajl <guicho.21.jq@gmail.com>
2023-01-27 18:32:55 -0600

    PythonCrashCourse

--- python_work/Ch_5/alien.py
+++ python_work/Ch_5/alien.py
@@ -41,7 +41,7 @@ alien_0['x_position'] = alien_0['x_position'] + x_increment
 
 print(f"New position: {alien_0['x_position']}")
 
-alien_0 = {'color':'green','points': 10}
+alien_0 = {'color':'green','points': 10,'speed' : 'medium'}
 print(alien_0)
 
 del alien_0['points']
