20d8fae2d4852ce624887c19a24d14ccd8d5f8b0
Quezadajl <guicho.21.jq@gmail.com>
2023-02-01 17:37:33 -0600

    Ch_6

--- python_work/Ch_6/intro_dictionaries.py
+++ python_work/Ch_6/intro_dictionaries.py
@@ -4,3 +4,7 @@ print(x)
 alien_0 = {'color':'green', 'points': 5}
 print(alien_0['color'])
 print(alien_0['points'])
+#using dictionaries to store key-value pairs
+
+new_points = alien_0['points']
+print(f"You just earned {new_points} points")
