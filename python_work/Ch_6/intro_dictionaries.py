17e53cb1e81339c31dbaaa6be13ced296a2ba489
Quezadajl <guicho.21.jq@gmail.com>
2023-02-01 17:44:17 -0600

    Ch_6

--- python_work/Ch_6/intro_dictionaries.py
+++ python_work/Ch_6/intro_dictionaries.py
@@ -8,3 +8,9 @@ print(alien_0['points'])
 
 new_points = alien_0['points']
 print(f"You just earned {new_points} points")
+#Redefined the dictionary & assigned it to a new variable
+
+#Adding new key-value pairs to the dictionary
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
