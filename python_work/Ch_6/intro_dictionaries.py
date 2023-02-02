dd0299e725898dcebd44570a0b714c1132b7522b
Quezadajl <guicho.21.jq@gmail.com>
2023-02-01 18:01:58 -0600

    Ch_6

--- python_work/Ch_6/intro_dictionaries.py
+++ python_work/Ch_6/intro_dictionaries.py
@@ -33,3 +33,9 @@ elif alien_0['speed'] == 'medium':
 else:
 #this must be a fast alien
 	x_increment = 3
+
+# The new position is the old position plus the increment
+
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+
+print(f"New position: {alien_0['x_position']}")
