0a4e102852101edd7e59d1e782dfe3f7eab77ace
Quezadajl <guicho.21.jq@gmail.com>
2023-02-01 18:44:31 -0600

    Ch_6

---
+++ python_work/Ch_6/favorite_languages.py
@@ -0,0 +1,9 @@
+#Creating a Dictionary from scratch of similar objects
+favorite_languages = {
+	'jen': 'python',
+	'sarah': 'c',
+	'edward': 'ruby',}
+
+print(favorite_languages)
+favorite_languages['phil'] = 'python'
+print(favorite_languages)
--- python_work/Ch_6/intro_dictionaries.py
+++ python_work/Ch_6/intro_dictionaries.py
@@ -47,12 +47,4 @@ del alien_0['points']
 print(alien_0)
 #deleted key-value pairs are removed permanently
 
-#Creating a Dictionary from scratch of similar objects
-favorite_languages = {
-	'jen': 'python',
-	'sarah': 'c',
-	'edward': 'ruby',}
 
-print(favorite_languages)
-favorite_languages['phil'] = 'python'
-print(favorite_languages)
