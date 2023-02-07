4717891f08aca04e18a847529f9f61c44728c0ec
Quezadajl <guicho.21.jq@gmail.com>
2023-02-06 19:42:55 -0600

    Ch_6

--- python_work/Ch_6/ch_6_challenge.py
+++ python_work/Ch_6/ch_6_challenge.py
@@ -4,3 +4,14 @@ print(luv['first_name'])
 print(luv['last_name'])
 print(luv['Age'])
 print(luv['city'])
+
+
+#6.2  Favorite Number
+fav_num = {'Martin':8,'Sophie':10,'Dolores':0.1,'Dante':7,'Mila':9}
+print(fav_num)
+
+print(f"Martin's favorite number is {fav_num['Martin']}")
+print(f"Sophie's favorite number is {fav_num['Sophie']}")
+print(f"Dolores's favorite number is {fav_num['Dolores']}")
+print(f"Mila's favorite number is {fav_num['Dante']}")
+print(f"Martin's favorite number is {fav_num['Mila']}")
