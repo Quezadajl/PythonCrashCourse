86c568fbde46eb0fffc37c073b750b95125c93a5
Quezadajl <guicho.21.jq@gmail.com>
2023-02-06 19:34:20 -0600

    Ch_6

--- python_work/Ch_6/ch_6_challenge.py
+++ python_work/Ch_6/ch_6_challenge.py
@@ -1,3 +1,6 @@
 #6.1  Person
-luv = {'first_name':'Tania','Last_name':'Davila','Age':29,'city':'Paris'}
-print(luv)
+luv = {'first_name':'Tania','last_name':'Davila','Age':29,'city':'Paris'}
+print(luv['first_name'])
+print(luv['last_name'])
+print(luv['Age'])
+print(luv['city'])
