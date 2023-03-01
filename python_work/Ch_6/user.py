ae6768ff7baf4dce2dcfa7a8745c964bca8e5c40
Quezadajl <guicho.21.jq@gmail.com>
2023-02-28 20:31:36 -0600

    Ch_6

---
+++ python_work/Ch_6/user.py
@@ -0,0 +1,11 @@
+user_0 = {
+	'username':'efermi',
+	'first':'enrico',
+	'last':'femi'
+}
+
+#Using  a for loop in a dictionary
+
+for key, value in user_0.items(): #include the name of a dictionary followed by method items()
+	print(f"\nKey:{key}")
+	print(f"Value:{value}")
