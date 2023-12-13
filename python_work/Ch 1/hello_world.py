771228801d2329bd93d223dfaca54c95b8c1bfa0
Quezadajl <guicho.21.jq@gmail.com>
2023-12-13 12:23:07 -0600

    python_work

--- python_work/Ch 1/hello_world.py
+++ python_work/Ch 1/hello_world.py
@@ -1,5 +1,5 @@
-message = "Hello Python World!"
+message = "Hello Python World! I am back again"
 print(message)
 
-message = "\tHello!\n\tPython\n\tCrash\n\tCourse\n\tReader!"
+message = "\tHello!\n\tPython\n\tCrash\n\tCourse\n\tReader!\n\tLets do this!\n\t100 days of code!"
 print(message.title())
