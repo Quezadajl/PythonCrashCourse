c44056ed2f5fcbdba1125ac38418c4e831db4083
Quezadajl <guicho.21.jq@gmail.com>
2023-12-13 21:04:56 -0600

    python_work

--- python_work/Ch 2/name_cases.py
+++ python_work/Ch 2/name_cases.py
@@ -1,17 +1,22 @@
  #Personal Message
-first_born = "Sophie"
-second_born = "Martin"
-third_born = "Lola"
-my_babies = f"Hello love of my life {first_born}, {second_born}, and {third_born}!"
-print(my_babies)
+first_born = "  Sophie "
+second_born = "    Martin"
+third_born = "Lola  "
+my_babies = f"   Hello love of my life {first_born}, {second_born}, and {third_born}!"
+print(first_born.strip())
+print(second_born.lstrip())
+print(third_born.strip())
+print(my_babies.strip())
 
 #Name cases
-first_name = "Tania"
-last_name = "Davila"
-full_name = f"{first_name} {last_name}"
-print(full_name.upper())
-print(full_name.lower())
-print(full_name.title())
+first_name = "Tania" # variable
+last_name = "Davila" #variable
+full_name = f"{first_name} {last_name}" # created a function to combine the variables
+
+print(full_name.upper()) #all capitalized
+print(full_name.lower()) #all lowercased
+print(full_name.title()) #First letter of each word capitalized
+print(my_babies.title().strip()) #testing multiple methods in the with strings
 
 #FamousQuote
 author = "Thor"
