26bcae9fddc59019c54244fc2aa20dd563e5de4f
Quezadajl <guicho.21.jq@gmail.com>
2023-12-19 22:36:46 -0600

    python_work

--- python_work/Ch 2/apostrophe.py
+++ python_work/Ch 2/apostrophe.py
@@ -1,6 +1,7 @@
 #Avoiding Syntax Errors
 message = "One of Python's strengths is its diverse community. - Joselit Questlove"
-print(message)
+
 #error
 #message = 'One of Python's strengths is its diverse community.'
 # the single quotes keep python from interpreting it correctly
+print(message)
