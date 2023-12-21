601975bc8748436d28934f094638f9b400e6e276
Quezadajl <guicho.21.jq@gmail.com>
2023-12-20 22:48:09 -0600

    python_work

--- python_work/Ch 3/motorcycles.py
+++ python_work/Ch 3/motorcycles.py
@@ -7,3 +7,16 @@ print(motorcycles)
 motorcycles[0] = 'BMW'
 print(motorcycles)#the code shows that we replaced the first element by re-defining a new list
 #Inserting Elements into a List
+
+my_doggies = [] #started a new list and will be adding elements to it
+my_doggies.append('Martin')
+my_doggies.append('Sophie')
+my_doggies.append('Alolamola')
+
+print(my_doggies)
+#Deleting and Inserting in an element from a specific place as long as you know it's index
+del my_doggies[2] #Use del and the index location to delete any element
+print(my_doggies)
+
+my_doggies.insert(2,'Lola') #Like del, you can use insert with the index number to add to a specific location
+print(my_doggies)
