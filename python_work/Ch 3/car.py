99551420e50c192b21bb8253902688aa7568306b
Quezadajl <guicho.21.jq@gmail.com>
2024-01-02 21:24:44 -0600

    python_work

--- python_work/Ch 3/car.py
+++ python_work/Ch 3/car.py
@@ -1,20 +1,20 @@
 cars = ['bmw', 'audi', 'toyota', 'subaru'] #upper or lower case can impact how the list is sorted permenantly
-#cars.sort()
+cars.sort()
 print(cars)
 #Python's sort() method makes it relatively easy to sort a list
 #cars.sort(reverse=True) #the reverse argument reverses ABD order
-print(cars)
-print("Here is the original list:")
-print(cars)
-print('\nHere is the sorted list:')#The sorted function is a temporary sorting option
-print(sorted(cars))
-print("\nHere is the original list again")
-print(cars)
+#print(cars)
+#print("Here is the original list:")
+#print(cars)
+#print('\nHere is the sorted list:')#The sorted function is a temporary sorting option
+#print(sorted(cars))
+#print("\nHere is the original list again")
+#print(cars)
 
 #Reverse chronological order
-cars.reverse()
-print(cars)
+#cars.reverse()
+#print(cars)
 
 #finding the length of a list
-length = len(cars)
-print(length)
+#length = len(cars)
+#print(length)
