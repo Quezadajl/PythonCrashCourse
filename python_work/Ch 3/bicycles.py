9d1a129a5806754a65cacda0e530f3661fafcb33
Quezadajl <guicho.21.jq@gmail.com>
2024-01-03 19:13:34 -0600

    python_work

--- python_work/Ch 3/bicycles.py
+++ python_work/Ch 3/bicycles.py
@@ -9,8 +9,8 @@ print(bicycles[-1].upper())# index -1 always returns the last item on the list
 message=f"\nMy first bicycle was a {bicycles[2].title()}."#using f-strings in messages w/indexes
 print(message)
 ###Excersis 3-1##
-#names = ['martin','sophie','lola','Tania']
-#print(names[0])
+names = ['martin','sophie','lola','Tania']
+print(names[0])
 ##greetings##
 #greetings = f"{names[2]} really likes to follow {names[0]} when he chases the {bicycles[-4]} while {names[1]} just watches"
 #print(greetings.title())
