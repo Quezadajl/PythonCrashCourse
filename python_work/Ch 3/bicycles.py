dc4274dd9ab31419f4c0339341c3fac3b12b538c
Quezadajl <guicho.21.jq@gmail.com>
2024-01-03 19:07:13 -0600

    python_work

--- python_work/Ch 3/bicycles.py
+++ python_work/Ch 3/bicycles.py
@@ -1,20 +1,20 @@
 bicycles = ['trek','cannondale','redline', 'specialized']
 print(bicycles)
-print(bicycles[0])
+#print(bicycles[0])
 #accessing any element in a list by telling python the position, or INDEX starting at 0
 #mix index with formating
-print(bicycles[0].title())
-print(bicycles[-1].upper())# index -1 always returns the last item on the list
+#print(bicycles[0].title())
+#print(bicycles[-1].upper())# index -1 always returns the last item on the list
 ####
-message=f"My first bicycle was a {bicycles[2].title()}."#using f-strings in messages w/indexes
-print(message)
+#message=f"My first bicycle was a {bicycles[2].title()}."#using f-strings in messages w/indexes
+#print(message)
 ###Excersis 3-1##
-names = ['martin','sophie','lola','Tania']
-print(names[0])
+#names = ['martin','sophie','lola','Tania']
+#print(names[0])
 ##greetings##
-greetings = f"{names[2]} really likes to follow {names[0]} when he chases the {bicycles[-4]} while {names[1]} just watches"
-print(greetings.title())
+#greetings = f"{names[2]} really likes to follow {names[0]} when he chases the {bicycles[-4]} while {names[1]} just watches"
+#print(greetings.title())
 
 #Practice
-all_is_good = f"{names[0]} on the couch, {names[1]} on her beanbag, and {names[-2]} sorrounded by shoes\n\t...all is good...\n\t at home"
-print(all_is_good)
+#all_is_good = f"{names[0]} on the couch, {names[1]} on her beanbag, and {names[-2]} sorrounded by shoes\n\t...all is good...\n\t at home"
+#print(all_is_good)
