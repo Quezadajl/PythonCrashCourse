a649add0c4ebc27f9060202a04a5b85296edcd7e
Quezadajl <guicho.21.jq@gmail.com>
2024-01-03 19:12:37 -0600

    python_work

--- python_work/Ch 3/bicycles.py
+++ python_work/Ch 3/bicycles.py
@@ -1,13 +1,13 @@
 bicycles = ['trek','cannondale','redline', 'specialized']
-print(bicycles)
-#print(bicycles[0])
+#print(bicycles)
+print(bicycles[0])
 #accessing any element in a list by telling python the position, or INDEX starting at 0
 #mix index with formating
-#print(bicycles[0].title())
-#print(bicycles[-1].upper())# index -1 always returns the last item on the list
+print(bicycles[0].title())
+print(bicycles[-1].upper())# index -1 always returns the last item on the list
 ####
-#message=f"My first bicycle was a {bicycles[2].title()}."#using f-strings in messages w/indexes
-#print(message)
+message=f"\nMy first bicycle was a {bicycles[2].title()}."#using f-strings in messages w/indexes
+print(message)
 ###Excersis 3-1##
 #names = ['martin','sophie','lola','Tania']
 #print(names[0])
