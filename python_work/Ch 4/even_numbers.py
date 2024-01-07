e3a725bb6731e1e916dcb198a9a0cb49377c56d9
Quezadajl <guicho.21.jq@gmail.com>
2024-01-06 19:28:08 -0600

    python_work

--- python_work/Ch 4/even_numbers.py
+++ python_work/Ch 4/even_numbers.py
@@ -1,3 +1,3 @@
 #Using range() to tell Python to skip numbers
-even_numbers = list(range(2,11,2))
+even_numbers = list(range(4,11,2)) #the 2, is the start off point, 11 is the end point, & the last 2 is the multiple sets
 print(even_numbers)
