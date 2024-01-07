09fbcc877cd4256303eee36621dff9490bb15fe3
Quezadajl <guicho.21.jq@gmail.com>
2024-01-06 19:45:45 -0600

    python_work

--- python_work/Ch 4/first_numbers.py
+++ python_work/Ch 4/first_numbers.py
@@ -4,12 +4,12 @@ for value in range(1,5):
 	#The range() function causes Python to start counting at the first value you give it, 
 	#  and stops when it reaches the 2nd value
 
-#for values in range(6):
-#	print(values)
+for values in range(6):
+	print(values)
 	#with only one argument it returns the numbers in between 0 to 5
 #Using range() to make a list of numbers using the list() function
-#numbers = list(range(7))
-#print(numbers)
+numbers = list(range(7))
+print(numbers)
 
 #creating list that skip-count
 #even_numbers = list(range(3,15,2))
