5d48929e4e2849401c9096a12d7a106fe5913d5d
Quezadajl <guicho.21.jq@gmail.com>
2024-01-17 10:47:13 -0600

    python_work

--- python_work/Ch 4/try_it_challenges.py
+++ python_work/Ch 4/try_it_challenges.py
@@ -1,31 +1,31 @@
 #4.3
-count = [value for value in range(1,21)]
-#print(count)
+count = [value for value in range(1,21)] #range here starts at 1 and ends before 21
+print(count)
 
 #4.4 & 4.5
 milli = [value for value in list(range(1,1000001))]
 #print(milli)
-#print(min(milli))
-#print(max(milli))
-#print(sum(milli))
+print(min(milli))
+print(max(milli))
+print(sum(milli))
 
 #4.6
-odd = [value for value in range(1,21,2)]
-print(odd)
+#odd = [value for value in range(1,21,2)]
+#print(odd)
 
-for value in range(1,21,2):
-	print(value)
+#for value in range(1,21,2):
+#	print(value)
 
 #4.7 & 4.8
-threes = [value for value in range(3,33,3)]
-print(threes)
+#threes = [value for value in range(3,33,3)]
+#print(threes)
 
 #4.9 & 4.10
-cubes = [value**3 for value in range(1,11)]
-print(cubes)
+#cubes = [value**3 for value in range(1,11)]
+#print(cubes)
 
-cubes_2 = []
-for cube in range(1,11):
-	cubes_2.append(cube**3)
-print(cubes_2)
+#cubes_2 = []
+#for cube in range(1,11):
+#	cubes_2.append(cube**3)
+#print(cubes_2)
 
