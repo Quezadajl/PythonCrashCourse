32e3a4a0c9d73e7f61790e8df654c2780f63d520
Quezadajl <guicho.21.jq@gmail.com>
2024-01-17 14:33:11 -0600

    python_work

--- python_work/Ch 4/try_it_challenges.py
+++ python_work/Ch 4/try_it_challenges.py
@@ -10,19 +10,19 @@ print(max(milli))
 print(sum(milli))
 
 #4.6
-#odd = [value for value in range(1,21,2)]
-#print(odd)
+odd = [value for value in range(1,30,2)]
+print(odd)
 
-#for value in range(1,21,2):
-#	print(value)
+for value in range(1,14,2):
+	print(value)
 
 #4.7 & 4.8
-#threes = [value for value in range(3,33,3)]
-#print(threes)
+threes = [value for value in range(3,33,3)]
+print(threes)
 
 #4.9 & 4.10
-#cubes = [value**3 for value in range(1,11)]
-#print(cubes)
+cubes = [value**3 for value in range(1,11)] #here we are using exponents to the 3rd power
+print(cubes)
 
 #cubes_2 = []
 #for cube in range(1,11):
