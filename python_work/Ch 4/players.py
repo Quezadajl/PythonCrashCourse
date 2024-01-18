70524a671ee433945ef13854ea8860c73b77497e
Quezadajl <guicho.21.jq@gmail.com>
2024-01-18 09:49:22 -0600

    python_work

--- python_work/Ch 4/players.py
+++ python_work/Ch 4/players.py
@@ -1,11 +1,11 @@
 #Slicing, outputting a specific elements in a list
-players = ['charles', 'martina','michael','florence','eli']
+players = ['charles', 'martina','michael','florence','eli','robert','david']
 print(players[0:3]) #Here we recall the first three elements
 print(players[:3]) # omitting the first index, python starts from the first element
 print(players[2:])# you can also start with the third element and index all the way through
-print(players[-3:])#using a negative pulls the last elements of the list
-#print(players[0:3:2])#A third number allows you to skip slice the list
-#print(players)
+print(players[-1:])#using a negative pulls the last elements of the list
+print(players[0::3])#A third number allows you to skip slice the list
+print(players)
 
 
 #Using Looping through a slice
