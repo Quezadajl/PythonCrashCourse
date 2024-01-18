ecae1d61a74bc2fb342d7eb381d1e7cf77801597
Quezadajl <guicho.21.jq@gmail.com>
2024-01-18 13:52:10 -0600

    python_work

--- python_work/Ch 4/magicians.py
+++ python_work/Ch 4/magicians.py
@@ -3,29 +3,29 @@ for magician in magicians: #use the for loop as "for every magician in the list 
 	print(magician)
 # We tell python to print the name that's just been assigned to the variable magician.
 
-babies = ['Martini-whinnie','Sophilicious','Alolamola']
-for doggies in babies:#doggies is the variable I assigned to each print of the list babies
-	print(doggies)
+#babies = ['Martini-whinnie','Sophilicious','Alolamola']
+#for doggies in babies:#doggies is the variable I assigned to each print of the list babies
+#	print(doggies)
 
-magicians = ['alice','david','carolina'] #Defining the list
-for magician in magicians: #use the for loop as "for every magician in the list of magicians, print the magician's name"
-	print(f'{magician.title()}, that was a great trick!')
-	print(f"I can't wait to see your next trick, {magician.title()}.\n")
-print("Thank you, everyone. That was a great magic show!")# Using print without the indentation does not go with the loop
+#magicians = ['alice','david','carolina'] #Defining the list
+#for magician in magicians: #use the for loop as "for every magician in the list of magicians, print the magician's name"
+#	print(f'{magician.title()}, that was a great trick!')
+#	print(f"I can't wait to see your next trick, {magician.title()}.\n")
+#print("Thank you, everyone. That was a great magic show!")# Using print without the indentation does not go with the loop
 
 #4.1 Challenge
-pizza = ['pepperoni', 'pinapple','cheese']
-for flavors in pizza:
-	print(f'I love {flavors} pizza!\n')
-print(f'I can eat {pizza[0]} pizza all the time, but the {pizza[1]} pizza with jalapenos is my favorite!')
+#pizza = ['pepperoni', 'pinapple','cheese']
+#for flavors in pizza:
+#	print(f'I love {flavors} pizza!\n')
+#print(f'I can eat {pizza[0]} pizza all the time, but the {pizza[1]} pizza with jalapenos is my favorite!')
 
 #4.2 Challenge
-animals = ['dog', 'dragon', 'plant']
-for pets in animals:
-	print(f'I believe that a {pets} make the best pets!\n\t')
+#animals = ['dog', 'dragon', 'plant']
+#for pets in animals:
+#	print(f'I believe that a {pets} make the best pets!\n\t')
 
 #Practice
-artists = ['Moderatto', 'The Killers', 'Taylor Swift']
-for artist in artists:
-	print(f'I can listen to {artist} all day!\n\t')
+#artists = ['Moderatto', 'The Killers', 'Taylor Swift']
+#for artist in artists:
+#	print(f'I can listen to {artist} all day!\n\t')
 
