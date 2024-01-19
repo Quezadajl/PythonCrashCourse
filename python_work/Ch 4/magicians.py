30fa6da442a90db72f5ab7d28d3e7826fd644c18
Quezadajl <guicho.21.jq@gmail.com>
2024-01-19 14:02:34 -0600

    python_work

--- python_work/Ch 4/magicians.py
+++ python_work/Ch 4/magicians.py
@@ -4,12 +4,12 @@ for magician in magicians: #use the for loop as "for every magician in the list 
 # We tell python to print the name that's just been assigned to the variable magician.
 
 babies = ['Martini-whinnie','Sophilicious','Lomitalomation']
-for doggies in babies:#doggies is the variable I assigned to each print of the list babies
+for doggies in babies: #doggies is the variable I assigned to each print of the list babies
 	print(doggies)
 
-#magicians = ['alice','david','carolina'] #Defining the list
-#for magician in magicians: #use the for loop as "for every magician in the list of magicians, print the magician's name"
-#	print(f'{magician.title()}, that was a great trick!')
+magicians = ['alice','david','carolina'] #Defining the list
+for magician in magicians: #use the for loop as "for every magician in the list of magicians, print the magician's name"
+	print(f'\n{magician.title()}, that was a great trick!\n')
 #	print(f"I can't wait to see your next trick, {magician.title()}.\n")
 #print("Thank you, everyone. That was a great magic show!")# Using print without the indentation does not go with the loop
 
