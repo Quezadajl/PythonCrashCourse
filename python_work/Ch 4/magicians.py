5b40831c0712a28e2df2fee7bc687cd2af465da2
Quezadajl <guicho.21.jq@gmail.com>
2024-01-19 14:10:10 -0600

    python_work

--- python_work/Ch 4/magicians.py
+++ python_work/Ch 4/magicians.py
@@ -6,12 +6,14 @@ for magician in magicians: #use the for loop as "for every magician in the list 
 babies = ['Martini-whinnie','Sophilicious','Lomitalomation']
 for doggies in babies: #doggies is the variable I assigned to each print of the list babies
 	print(f'\n{doggies} is the best at cuddling\n')
+	print(f'\n{doggies} you are the best!\n')
+print("thank you for being so unique in personalities")
 
 magicians = ['alice','david','carolina'] #Defining the list
 for magician in magicians: #use the for loop as "for every magician in the list of magicians, print the magician's name"
 	print(f'\n{magician.title()}, that was a great trick!\n')
-#	print(f"I can't wait to see your next trick, {magician.title()}.\n")
-#print("Thank you, everyone. That was a great magic show!")# Using print without the indentation does not go with the loop
+	print(f"I can't wait to see your next trick, {magician.title()}.\n")
+print("Thank you, everyone. That was a great magic show!")# Using print without the indentation does not go with the loop
 
 #4.1 Challenge
 #pizza = ['pepperoni', 'pinapple','cheese']
