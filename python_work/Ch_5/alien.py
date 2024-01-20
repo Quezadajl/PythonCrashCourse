44f410bc78727b55129d29529fb1e6c849e6003c
Quezadajl <guicho.21.jq@gmail.com>
2024-01-20 13:48:34 -0600

    python_work

--- python_work/Ch_5/alien.py
+++ python_work/Ch_5/alien.py
@@ -1,23 +1,23 @@
 alien_0 = {'color':'green','points': 5} # using braces with semicolons to store information; Dictionary
-print(alien_0['color'])
-print(alien_0['points'])
+#print(alien_0['color'])
+#print(alien_0['points'])
 
 new_points = alien_0['points']
-print(f"You just earned {new_points} points!")
+#print(f"You just earned {new_points} points!")
 
 #print(alien_0)
 
 alien_0['x_position'] = 0 #screen coordinates usually start at the upper-left corner of the screen
 alien_0['y_position'] = 25 #Give the name of the dictionary followed by the new key in [ ] brackets
-print(alien_0)
+#print(alien_0)
 
 #starting with an empty dictionary
-#alien_0 = {}
+alien_0 = {}
 
-#alien_0['color'] = 'green'
-#alien_0['points'] = 5
+alien_0['color'] = 'green'
+alien_0['points'] = 5
 
-#print(alien_0)
+print(alien_0)
 #print(f"The alien is now {alien_0['color']}.")
 
 #alien_0['color'] = 'yellow' #modifying values in a dictionary
