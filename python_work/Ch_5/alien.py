625a2e23b6289614e049774c3692b469ed87be40
Quezadajl <guicho.21.jq@gmail.com>
2024-01-20 12:35:35 -0600

    python_work

--- python_work/Ch_5/alien.py
+++ python_work/Ch_5/alien.py
@@ -7,9 +7,9 @@ print(f"You just earned {new_points} points!")
 
 #print(alien_0)
 
-#alien_0['x_position'] = 0 #screen coordinates usually start at the upper-left corner of the screen
+alien_0['x_position'] = 0 #screen coordinates usually start at the upper-left corner of the screen
 #alien_0['y_position'] = 25 #Give the name of the dictionary followed by the new key in [ ] brackets
-#print(alien_0)
+print(alien_0)
 
 #starting with an empty dictionary
 #alien_0 = {}
