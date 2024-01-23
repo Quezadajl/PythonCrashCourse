975ed46bacabc90f3a7f7543b1a192464cce9411
Quezadajl <guicho.21.jq@gmail.com>
2024-01-22 21:03:47 -0600

    python_work

--- python_work/Ch_5/Challenge_Ch_5.py
+++ python_work/Ch_5/Challenge_Ch_5.py
@@ -1,37 +1,37 @@
 #Hello Admin challenge
-usernames = ['totodile','Cyndaquil','Chikorita','Rowlet','Admin','Chimpchar'] #Creating a dictionary
-root_user = ['Admin']
+#usernames = ['totodile','Cyndaquil','Chikorita','Rowlet','Admin','Chimpchar'] #Creating a dictionary
+#root_user = ['Admin']
 #usernames = []
-if 'totodile' in usernames:
-	print(f'Hello! thank you for logging in again.')
-if 'Cyndaquil' in usernames:
-	print(f'Hello!, thank you for logging in again.')
-if 'Rowlet' in usernames:
-	print(f'Hello! thank you for logging in again.')
-if 'Chimpchar' in usernames:
-	print(f'Hello!, thank you for logging in again')
-if 'Admin' in usernames:
-	print('Hello admin, would you like to see a status report?')
-if usernames is []:
-	print('We need to find some users!')
+#if 'totodile' in usernames:
+#	print(f'Hello! thank you for logging in again.')
+#if 'Cyndaquil' in usernames:
+#	print(f'Hello!, thank you for logging in again.')
+#if 'Rowlet' in usernames:
+#	print(f'Hello! thank you for logging in again.')
+#if 'Chimpchar' in usernames:
+#	print(f'Hello!, thank you for logging in again')
+#if 'Admin' in usernames:
+#	print('Hello admin, would you like to see a status report?')
+#if usernames is []:
+#	print('We need to find some users!')
 
-for root in root_user:
-	if root in usernames:
-		print('Hello Admin, would you like to see the status of the report')
-	if root not in usernames:
-		print('Hello! thank you for logging in again')
-	else:
-		print('We need to find some users!')
-#Next Challenge
-#current_users = ['Mario','Luigi','Pricess Peach','Toad','Yoshi']
-#new_users = ['Mario','luigi','Princess Daisy', 'Rosalina','yoshi']
-
-#for new_user in new_users:
-#	if new_user.title() in current_users: #using .title() allows for the code to check for upper/lower case
-#		print(new_user,'is NOT available, must enter an new username') #using the variable w/o 'f' or ' ' 
-		#helps make the output loop through each name individually and post it as its own line!!#mindblown
+#for root in root_user:
+#	if root in usernames:
+#		print('Hello Admin, would you like to see the status of the report')
+#	if root not in usernames:
+#		print('Hello! thank you for logging in again')
 #	else:
-#		print(new_user, 'is available')
+#		print('We need to find some users!')
+#Next Challenge
+current_users = ['Mario','Luigi','Pricess Peach','Toad','Yoshi']
+new_users = ['Mario','luigi','Princess Daisy', 'Rosalina','yoshi']
+
+for new_user in new_users:
+	if new_user.title() in current_users: #using .title() allows for the code to check for upper/lower case
+		print(new_user,'is NOT available, must enter an new username') #using the variable w/o 'f' or ' ' 
+		#helps make the output loop through each name individually and post it as its own line!!#mindblown
+	else:
+		print(new_user, 'is available')
 
 #Next challenge
 #num = [1,2,3,4,5,6,7,8,9]
