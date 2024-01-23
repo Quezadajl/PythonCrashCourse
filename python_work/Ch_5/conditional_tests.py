530f56912d51fffc06380e590f9defd004b68c2b
Quezadajl <guicho.21.jq@gmail.com>
2024-01-22 21:13:30 -0600

    python_work

--- python_work/Ch_5/conditional_tests.py
+++ python_work/Ch_5/conditional_tests.py
@@ -4,13 +4,13 @@ snacks = ['peanuts','chocolate','doritos','popcorn']
 favorite_snack = 'doritos'
 
 #lets print snacks out, but let everyone know my favorite snack
-#for snack in snacks:
-#	if snack == favorite_snack:
+for snack in snacks:
+	if snack == favorite_snack:
 		#this snack is my favorite, let's let everyone know!
-#		print("%s is my favorite snack!" % snack.title()) # -- it looks like the % is used as a variable with additional formating
-#	else:
+		print("%s is my favorite snack!" % snack.title()) # -- it looks like the % is used as a variable with additional formating
+	else:
 		#I like these other snacks, but not my favorite
-#		print("I like %s." % snack)
+		print("I like %s." % snack)
 
 ## other Tests for inequality and equality
 #requested_topping = 'pineapple'
