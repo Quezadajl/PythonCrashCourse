694de93c887087490c88dd55b56617fcaa381cbe
Quezadajl <guicho.21.jq@gmail.com>
2024-01-23 20:02:17 -0600

    python_work

--- python_work/Ch_5/conditional_tests.py
+++ python_work/Ch_5/conditional_tests.py
@@ -4,43 +4,44 @@ snacks = ['peanuts','chocolate','doritos','popcorn']
 favorite_snack = 'doritos'
 
 #lets print snacks out, but let everyone know my favorite snack
-for snack in snacks:
-	if snack == favorite_snack:
+#for snack in snacks:
+#	if snack == favorite_snack:
 		#this snack is my favorite, let's let everyone know!
-		print("%s is my favorite snack!" % snack.title()) # -- it looks like the % is used as a variable with additional formating
-	else:
+#		print("%s is my favorite snack!" % snack.title()) # -- it looks like the % is used as a variable with additional formating
+#	else:
 		#I like these other snacks, but not my favorite
-		print("I like %s." % snack)
+#		print("I like %s." % snack)
 
 ## other Tests for inequality and equality
-requested_topping = 'pineapple'
-if requested_topping != 'jalapenos':
-	print("Hold the jalapenos!")
+#requested_topping = 'pineapple'
+#if requested_topping != 'jalapenos':
+#	print("Hold the jalapenos!")
 
-needed_ingredients = ['doritos','popcorn','chocolate','fruit'] #Here I'm checking if these items are in the pantry
-avail_ingredient = 'chocolate'
+needed_ingredients = ['doritos','popcorn','chocolate','fruit'] 
+#Here I'm checking if these items are in the pantry
+avail_ingredient = 'doritos'
 
 for treat in needed_ingredients:
 	if treat == avail_ingredient:
 		#this snack is my favorite, let's let everyone know!
-		print("Great we have %s!" % treat.title()) # -- it looks like the % is used as a variable with additional formating
+		print("Great! we have %s!" % treat.title()) # -- it looks like the % is used as a variable with additional formating
 	else:
 		#I like these other snacks, but not my favorite
 		print("We will need %s from the store." % treat)
 
 #Trying other conditional tests: if-else statements and chains
-#dogs = ['Dante','Myla','Bunior(cat)']
+dogs = ['Dante','Myla','Bunior(cat)']
 
-#if len(dogs) >= 3:
-#	print("Wow, we have a lot of dogs here!")
-#else:
-#	print("Okay, this is a reasonable number of dogs.")
+if len(dogs) >= 4:
+	print("Wow, we have a lot of dogs here!")
+else:
+	print("Okay, this is a reasonable number of dogs.")
 
 #and if-else chains
-#dogs = ['Sophie','Martin','Lola','Chuying','Guegene']
+dogs = ['Sophie','Martin','Lola','Chuying','Guegene']
 
-#if len(dogs) >= 5:
-#	print("Wow,we might start a dog hostel!")
+if len(dogs) >= 5:
+	print("Wow,we might start a dog hostel!")
 #elif len(dogs) >= 3:
 #	print("wow, we have a lot of dogs here!")
 #else:
