e9b546faf4005cec8348a11e2b5b88e13229de6d
Quezadajl <guicho.21.jq@gmail.com>
2024-01-25 21:29:20 -0600

    python_work

--- python_work/Ch_5/voting.py
+++ python_work/Ch_5/voting.py
@@ -108,14 +108,14 @@ else:
 	print("Are you sure you want a plain pizza?")
 
 
-#available_toppings = ['mushrooms','olives','green peppers','pepperoni','pineapple','extra cheese']
+available_toppings = ['mushrooms','olives','green peppers','pepperoni','pineapple','extra cheese']
 
-#requested_toppings = ['mushrooms','french fries','extra cheese','pineapple']
+requested_toppings = ['mushrooms','french fries','extra cheese','pineapple']
 
-#for requested_topping in requested_toppings:
-#	if requested_topping in available_toppings:
-#		print(f"Adding {requested_topping}.")
-#	else:
-#		print(f"Sorry, we don't have {requested_topping}.")
-#print("\nFinished making your pizza!!!")
+for requested_topping in requested_toppings:
+	if requested_topping in available_toppings:
+		print(f"Adding {requested_topping}.")
+	else:
+		print(f"Sorry, we don't have {requested_topping}.")
+print("\nFinished making your pizza!!!")
 
