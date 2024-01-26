1626fc3c037ce1e891fdcb3ea82d7acebfc79435
Quezadajl <guicho.21.jq@gmail.com>
2024-01-25 21:17:15 -0600

    python_work

--- python_work/Ch_5/voting.py
+++ python_work/Ch_5/voting.py
@@ -95,17 +95,17 @@ if 'extra cheese' in requested_toppings:
 
 print("\nFinished making your pizza!")
 
-#requested_toppings = []
+requested_toppings = []
 
-#if requested_toppings:
-#	for requested_topping in requested_toppings:
-#		if requested_topping == 'green peppers':
-#			print("Sorry, we are out of green peppers right now.")
-#		else:
-#			print(f"Adding {requested_topping}.")
-#	print("\nFinished making your pizza!!")
-#else:
-#	print("Are you sure you want a plain pizza?")
+if requested_toppings:
+	for requested_topping in requested_toppings:
+		if requested_topping == 'green peppers':
+			print("Sorry, we are out of green peppers right now.")
+		else:
+			print(f"Adding {requested_topping}.")
+	print("\nFinished making your pizza!!")
+else:
+	print("Are you sure you want a plain pizza?")
 
 
 #available_toppings = ['mushrooms','olives','green peppers','pepperoni','pineapple','extra cheese']
