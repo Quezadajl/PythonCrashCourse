8e4db6e21f705c98c663c9f13a30f651b6e569ed
Quezadajl <guicho.21.jq@gmail.com>
2024-01-25 21:02:38 -0600

    python_work

--- python_work/Ch_5/voting.py
+++ python_work/Ch_5/voting.py
@@ -88,10 +88,10 @@ if 'mango' in favorite_fruits:
 requested_toppings = ['mushrooms','green peppers','extra cheese']
 if 'mushrooms' in requested_toppings:
 	print('Adding mushrooms.')
-#if 'pepperoni' in requested_toppings:
-#	print('Adding pepperoni.')
-#if 'extra cheese' in requested_toppings:
-#	print('Adding extra cheese.')
+if 'pepperoni' in requested_toppings:
+	print('Adding pepperoni.')
+if 'extra cheese' in requested_toppings:
+	print('Adding extra cheese.')
 
 #print("\nFinished making your pizza!")
 
