f2654ba7102d49a8044bcd750684b0200dbed281
Quezadajl <guicho.21.jq@gmail.com>
2024-01-26 20:45:22 -0600

    python_work

--- python_work/Ch_6/ch_6_challenge.py
+++ python_work/Ch_6/ch_6_challenge.py
@@ -1,32 +1,32 @@
 #6.1  Person
 luv = {'first_name':'Tania','last_name':'Davila','Age':29,'city':'Paris'}
 print(luv['first_name'])
-print(luv['last_name'])
-print(luv['Age'])
-print(luv['city'])
+#print(luv['last_name'])
+#print(luv['Age'])
+#print(luv['city'])
 
 
 #6.2  Favorite Number
-fav_num = {'Martin':8,'Sophie':10,'Dolores':0.1,'Dante':7,'Mila':9}
-print(fav_num)
+#fav_num = {'Martin':8,'Sophie':10,'Dolores':0.1,'Dante':7,'Mila':9}
+#print(fav_num)
 
-print(f"Martin's favorite number is {fav_num['Martin']}")
-print(f"Sophie's favorite number is {fav_num['Sophie']}")
-print(f"Dolores's favorite number is {fav_num['Dolores']}")
-print(f"Mila's favorite number is {fav_num['Dante']}")
-print(f"Martin's favorite number is {fav_num['Mila']}")
+#print(f"Martin's favorite number is {fav_num['Martin']}")
+#print(f"Sophie's favorite number is {fav_num['Sophie']}")
+#print(f"Dolores's favorite number is {fav_num['Dolores']}")
+#print(f"Mila's favorite number is {fav_num['Dante']}")
+#print(f"Martin's favorite number is {fav_num['Mila']}")
 
 #6.3  Glossary
 
-glossary = {'string': 'A series of characters.',
-    'comment': 'A note in a program that the Python interpreter ignores.',
-    'list': 'A collection of items in a particular order.',
-    'loop': 'Work through a collection of items, one at a time.',
-    'dictionary': "A collection of key-value pairs.",}
+#glossary = {'string': 'A series of characters.',
+ #   'comment': 'A note in a program that the Python interpreter ignores.',
+ #   'list': 'A collection of items in a particular order.',
+ #   'loop': 'Work through a collection of items, one at a time.',
+ #   'dictionary': "A collection of key-value pairs.",}
 #print(glossary)
 
-print(f"String:{glossary['string']}")
-print(f"\n comment: {glossary['comment']}")
-print(f"\n lists: {glossary['list']}")
-print(f"\n loop: {glossary['loop']}")
-print(f"\n dictionary:{glossary['dictionary']}")
+#print(f"String:{glossary['string']}")
+#print(f"\n comment: {glossary['comment']}")
+#print(f"\n lists: {glossary['list']}")
+#print(f"\n loop: {glossary['loop']}")
+#print(f"\n dictionary:{glossary['dictionary']}")
