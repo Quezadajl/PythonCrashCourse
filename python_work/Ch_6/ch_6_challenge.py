840c50ec3dd9b5a4ba068020c82482d78e9b260a
Quezadajl <guicho.21.jq@gmail.com>
2024-01-26 20:49:34 -0600

    python_work

--- python_work/Ch_6/ch_6_challenge.py
+++ python_work/Ch_6/ch_6_challenge.py
@@ -1,14 +1,15 @@
 #6.1  Person
 luv = {'first_name':'Tania','last_name':'Davila','Age':29,'city':'Paris'}
 print(luv['first_name'])
-#print(luv['last_name'])
-#print(luv['Age'])
-#print(luv['city'])
+print(luv['last_name'])
+print(luv['Age'])
+print(luv['city'])
 
 
 #6.2  Favorite Number
-#fav_num = {'Martin':8,'Sophie':10,'Dolores':0.1,'Dante':7,'Mila':9}
-#print(fav_num)
+fav_num = {'Martin':10,'Sophie':12,'Dolores':2,'Dante':9,'Mila':11}
+print(fav_num)
+print(fav_num['Martin'])
 
 #print(f"Martin's favorite number is {fav_num['Martin']}")
 #print(f"Sophie's favorite number is {fav_num['Sophie']}")
