b2e927d90c6ad4b23b7854f3d1c1c821c4b768cd
Quezadajl <guicho.21.jq@gmail.com>
2024-01-26 20:41:22 -0600

    python_work

--- python_work/Ch_5/banned_user.py
+++ python_work/Ch_5/banned_user.py
@@ -1,14 +1,14 @@
 banned_users = ['andrew','carolina','david']
 user = 'david'
-#user = 'marie'
+user = 'marie'
 
 if user not in banned_users:
 	print(f'{user.title()}, you can post a response if you wish.')
 # If teh value user is not in the list banned_users, Python returns True and executes the intended line.
 
 #Coditional Test 5-1
-#car = 'Subaru'
-#print("is car == 'subaru'? I predict True.")
-#print(car == 'subaru') # Case is important with conditional tests, hence why it's false if lower case
-#print("\nIs car == 'audi'? I predict False")
-#print(car == 'audi')
+car = 'Subaru'
+print("is car == 'subaru'? I predict True.")
+print(car == 'subaru') # Case is important with conditional tests, hence why it's false if lower case
+print("\nIs car == 'audi'? I predict False")
+print(car == 'audi')
