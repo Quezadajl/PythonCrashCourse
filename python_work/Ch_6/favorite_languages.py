d0097425f91755002df97cfcbe8079d21c6f8c19
Quezadajl <guicho.21.jq@gmail.com>
2024-01-29 22:25:03 -0600

    python_work

--- python_work/Ch_6/favorite_languages.py
+++ python_work/Ch_6/favorite_languages.py
@@ -11,7 +11,7 @@ print(favorite_games)
 game = favorite_games['Luisito'].title()
 game1 = favorite_games['juandiego'].title()
 print(f"Luisito's favorite game is {game}.\n")
-print(f"juandiego's favorite game is {game}.\n")
+print(f"juandiego's favorite game is {game1}.\n")
 
 
 for key, value in favorite_games.items(): #include the name of a dictionary followed by method items()
@@ -37,16 +37,16 @@ print(f"Sarah's favorite language is {language}.")
 #Using get() to Access Values
 # alien_no_points.py
 
-#alien_0 = {
-	#'color': 'green', 
-#	'speed':'slow', 
-#	'points':5,
-#	}
+alien_0 = {
+	'color': 'green', 
+	'speed':'slow', 
+	'points':0,
+	}
 #Working around the traceback error especifically for this, we have used the get method to bypass it
 #The get() method requires a key for first argument, and an optional second argument to return a value..
 #..if first argument is null
-#point_value = alien_0.get('points','No point value assigned.')
-#print(point_value)
+point_value = alien_0.get('points','No point value assigned.')
+print(point_value)
 
 #color_get = alien_0.get('color', 'No color value assigned')
 #print(color_get)
