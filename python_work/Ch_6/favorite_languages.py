e4b3ca8aac64fdb6e918b2d30e575f7f6f698996
Quezadajl <guicho.21.jq@gmail.com>
2024-02-01 21:37:04 -0600

    python_work

--- python_work/Ch_6/favorite_languages.py
+++ python_work/Ch_6/favorite_languages.py
@@ -65,8 +65,8 @@ person_0 = {
 print(person_0['first_name'])
 print(person_0['last_name'])
 print(person_0['age'])
-#print(person_0['city'])
-#print(person_0)
+print(person_0['city'])
+print(person_0)
 
 ###########################################
 
