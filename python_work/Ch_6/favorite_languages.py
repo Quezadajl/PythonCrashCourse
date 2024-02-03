b3017a5d9e66229bd5b55c76880881f696b0c5ad
Quezadajl <guicho.21.jq@gmail.com>
2024-02-02 21:46:55 -0600

    python_work

--- python_work/Ch_6/favorite_languages.py
+++ python_work/Ch_6/favorite_languages.py
@@ -113,6 +113,18 @@ print('for loops :')
 print(glossary['for loops'])
 
 ###########################################
+## 6.4
+#Now that you know how to loop through a dictionary, clean
+#up the code from Exercise 6-3 (page 102) by replacing your series of print
+#statements with a loop that runs through the dictionary’s keys and values.
+#When you’re sure that your loop works, add five more Python terms to your
+#glossary. When you run your program again, these new words and meanings
+#should automatically be included in the output.
+
+for word, definition in glossary.items():
+	print(word + " :\n" + definition + '\n')
+	
+###########################################
 
 #Using get() to Access Values
 # alien_no_points.py
