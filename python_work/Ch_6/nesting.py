2bec06a6b023f19e69948374aa3abd212e0d5202
Quezadajl <guicho.21.jq@gmail.com>
2024-02-04 16:29:29 -0600

    python_work

--- python_work/Ch_6/nesting.py
+++ python_work/Ch_6/nesting.py
@@ -8,3 +8,17 @@ aliens = [alien_0,alien_1,alien_2] # here is where the nesting takes place; a li
 #loop through it
 for alien in aliens:
  print(alien)
+
+#In reality, lets automate this using range()
+#Make an empty list for storing aliens
+aliens = []
+
+#Make 30 green aliens
+for alien_number in range(30):
+	new_alien = {'color':'green', 'point':5}
+	aliens.append(new_alien)
+
+#Show the first 5 aliens
+for alien in aliens[:5]:
+	print(alien)
+print("...")
