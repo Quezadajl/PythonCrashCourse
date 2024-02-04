a50be56fd01b60ebcaab63fd3a8e01f8186dcab1
Quezadajl <guicho.21.jq@gmail.com>
2024-02-04 16:24:15 -0600

    python_work

---
+++ python_work/Ch_6/nesting.py
@@ -0,0 +1,8 @@
+#Nesting: a lits of items inside a dictionary
+alien_0 = {'color':'green', 'point':5}
+alien_1 = {'color':'yellow', 'point':10}
+alien_2 = {'color':'red', 'point':15}
+
+aliens = [alien_0,alien_1,alien_2] # here is where the nesting takes place; a list of dictionaries
+
+print(aliens)
