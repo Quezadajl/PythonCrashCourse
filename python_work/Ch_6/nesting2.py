35fed57975c5f6b32aa2c8be4858e18f7fc6c060
Quezadajl <guicho.21.jq@gmail.com>
2024-02-09 21:58:16 -0600

    python_work

--- python_work/Ch_6/nesting2.py
+++ python_work/Ch_6/nesting2.py
@@ -12,11 +12,11 @@ users = {
 	'location':'paris',
 	},
 }
-
-for username, user_info in users.items(): #
-	print(f"\nUsername: {username}") #
-	full_name = f"{user_info['first']} {user_info['last']}" #
-	location = user_info['location'] #
+# we define a dict with 2 keys, each key is a dict with user info
+for username, user_info in users.items(): # we loop through the user's info
+	print(f"\nUsername: {username}") # print the username info
+	full_name = f"{user_info['first']} {user_info['last']}" # accessing inner dict, the last three keys
+	location = user_info['location'] # print summary
 
 	print(f"\tFull name: {full_name.title()}")
 	print(f"\tLocation: {location.title()}")
