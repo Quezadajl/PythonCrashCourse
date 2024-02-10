22cfe2b6936f38694a50fe01100f81ea4f4b05d5
Quezadajl <guicho.21.jq@gmail.com>
2024-02-09 21:31:08 -0600

    python_work

--- python_work/Ch_6/nesting2.py
+++ python_work/Ch_6/nesting2.py
@@ -12,3 +12,8 @@ users = {
 	'location':'paris',
 	},
 }
+
+for username, user_info in users.items():
+	print(f"\nUsername: {username}")
+	full_name = f"{user_info['first']} {user_info['last']}"
+	location = user_info['location']
