54893a3c514769416706c8d1492ce34a6eafe98f
Quezadajl <guicho.21.jq@gmail.com>
2024-02-10 22:22:04 -0600

    python_work

--- python_work/Ch_6/nesting2.py
+++ python_work/Ch_6/nesting2.py
@@ -20,3 +20,28 @@ for username, user_info in users.items(): # we loop through the user's info
 
 	print(f"\tFull name: {full_name.title()}")
 	print(f"\tLocation: {location.title()}")
+
+## 6.7 People
+#Start with the program you wrote for Exercise 6-1 (page 102).
+#Make two new dictionaries representing different people, and store all three
+#dictionaries in a list called people. Loop through your list of people. As you
+#loop through the list, print everything you know about each person.
+
+#person_1 = {
+#	'first_name': 'William',
+#	'last_name': 'Martin',
+#	'age': 29,
+#	'city': 'Talmo'
+#}
+
+#person_2 = {
+#	'first_name': 'Taylor',
+#	'last_name': 'Martin',
+#	'age': 27,
+#	'city': 'Jefferson'
+#}
+
+#persons = [person_0, person_1, person_2]
+
+#for person in persons:
+#	print(person)
