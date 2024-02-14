748f8eebd208c9cb8bf6f9fe088a8513a62e75e6
Quezadajl <guicho.21.jq@gmail.com>
2024-02-13 21:22:11 -0600

    python_work

--- python_work/Ch_6/nesting2.py
+++ python_work/Ch_6/nesting2.py
@@ -80,4 +80,20 @@ pets = [Martin, Sophie, Lola]
 #for pet in pets:
 #	print(pet)
 
+## 6.9 Favorite Places
+#Make a dictionary called favorite_places. Think of three
+#names to use as keys in the dictionary, and store one to three favorite places
+#for each person. To make this exercise a bit more interesting, ask some friends
+#to name a few of their favorite places. Loop through the dictionary, and print
+#each person’s name and their favorite places.
 
+favorite_places = {
+#	'william': ['lake tahoe', 'maldives', 'talmo'],
+#	'allie': ['talmo', 'mexico', 'bahamas'],
+#	'luke': ['home', 'nanas', 'grams'],
+}
+
+#for name, places in favorite_places.items():
+#	print(name.title() + ", your favorite places are:")
+#	for place in places:
+#		print("\t" + place.title())
