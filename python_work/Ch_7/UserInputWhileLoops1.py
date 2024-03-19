e830346ead6a58f40c2afd66708943863b60d7a7
Quezadajl <guicho.21.jq@gmail.com>
2024-03-18 21:10:57 -0500

    python_work

--- python_work/Ch_7/UserInputWhileLoops1.py
+++ python_work/Ch_7/UserInputWhileLoops1.py
@@ -1,8 +1,14 @@
 #User Input as parrot.py
-message = input("Tell me something, and I will repeat it back to you: ")
+#message = input("Tell me something, and I will repeat it back to you: ")
 #print(message)
 #for these exercises, use your terminal to get the input prompt
 
 #greeter.py sample
-name = input("Please enter your name: ")
+#name = input("Please enter your name: ")
 #print(f"\nHello,{name}!")
+
+prompt = "If you tell us who you are, we can personalize the messages you see."
+prompt += "\nWhat is your first name? "
+
+name = input(prompt)
+print(f"\nHello, {name}!")
